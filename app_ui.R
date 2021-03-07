e557869c6a046ae6e1c43aa8caa3b6f2e83e91aa
cj.rieth@att.net <cj.rieth@att.net>
2021-03-07 15:15:26 -0800

    first go through

--- app_ui.R
+++ app_ui.R
@@ -2,15 +2,31 @@ library(dplyr)
 library(lintr)
 library(shiny)
 library(ggplot2)
+library(plotly)
+
 source("app_server.R")
 
 introduction <-
-  
-  
+
 page_one <-
-  
-  
-page_two <-
+
+gpu_page <- tabPanel(
+    "GPU Releases and Crypto Price",
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          inputId = "gpu_crypto",
+          label = "Cryptocurrency",
+          choices =  c("Bitcoin", "Ethereum", "Dash")
+        )
+      ),
+      mainPanel(
+        plotlyOutput(
+          outputId = "gpu"
+        )
+      )
+    )
+)
   
   
 page_three <-
@@ -19,4 +35,6 @@ page_three <-
 summary <- 
   
   
-ui <- navbarPage("Cryptocurrency Analysis", introduction, page_one, page_two, page_three, summary)
+ui <- navbarPage("Cryptocurrency Analysis", introduction, page_one, pgu_page, page_three, summary)
+
+
