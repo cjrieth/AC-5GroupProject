c34be79adb11a9ee8592375cf1ac802b45f75385
Anusha Jain <ajain20@uw.edu>
2021-03-14 20:05:29 -0700

    ui change

--- app_ui.R
+++ app_ui.R
@@ -11,12 +11,12 @@ introduction <-
 choose_crypto <- selectInput(inputId = "choose_crypto",
                                 label = h3("Choose a Cryptocurrency"),
                                 choices = "Bitcoin", "Dash", "Ethereum", "Iota")
-# choose_data <- selectInput("choose_data", label = h3("Choose a Type of Data"),
-#                            choices = "High", "Low", "Open", "Close", "Volume")  
+choose_data <- selectInput("choose_data", label = h3("Choose a Type of Data"),
+                           choices = "High", "Low", "Open", "Close", "Volume")  
 page_one <- tabPanel(
   "Cryptocurrency Prices During the Pandemic", sidebarLayout(
     sidebarPanel(
-      choose_crypto), #, choose_data),
+      choose_crypto, choose_data),
     mainPanel(
       plotlyOutput("crypto_vs_time"),
       p("blub"),
